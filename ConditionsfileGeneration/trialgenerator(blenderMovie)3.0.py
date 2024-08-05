91fad74f8791bb52df3420cda839f5d079c4da66
sebmonte <99094756+sebmonte@users.noreply.github.com>
2024-08-05 11:21:21 -0400

    New version of experiment script to test timing

--- ConditionsfileGeneration/trialgenerator(blenderMovie)3.0.py
+++ ConditionsfileGeneration/trialgenerator(blenderMovie)3.0.py
@@ -24,14 +24,13 @@ conditions = ['0022_Left', '0022_Right', '0067_Left', '0067_Right', '0112_Left',
               '0112_Right', '0157_Left', '0157_Right', '0202_Left', '0202_Right',
               '0247_Left', '0247_Right', '0292_Left', '0292_Right', '0337_Left', '0337_Right']
 
-# Create a dictionary to hold the groups
+# This creates a dictionary that maps the conditions that
+#end in the same places as others start. For instance '0022_Left'
+#Ends where '292_Right' starts and vice versa
 grouped_conditions = {}
-
-# Function to find the group key
 def find_group_key(num):
     return min(num, num - 180 if num >= 180 else num + 180)
 
-# Loop through the conditions and group them
 for condition in conditions:
     number, side = condition.split('_')
     num = int(number)
@@ -41,10 +40,18 @@ for condition in conditions:
         grouped_conditions[group_key] = []
     grouped_conditions[group_key].append(condition)
 
-# Convert the grouped dictionary to a list of lists
+# This is now a list where each sublist contains the conditions
+#Grouped by start and end positions being the same
 grouped_list = list(grouped_conditions.values())
 
 
+#This function is intended to check if a given trial is preceded by
+#A trial that ended where this trial begins. We want to have this
+#Not happen to avoid any effect of continuity 
+#We take in a list of trials for a given run in the experiment,
+#and also a list that shows what trials start and end in the same places
+#Returns true if any two trials in a run start or end in the same place
+#Otherwise returns false
 def validate_grouping1(input_list, grouped_list):
     # Create a mapping from condition to its index in the input list
     index_map = {condition: index for index, condition in enumerate(input_list)}
@@ -63,6 +70,10 @@ def validate_grouping1(input_list, grouped_list):
     return False
 
 
+#This also checks whether one trial is next to another such that
+#it ends where another trial begins. The difference is that this function
+#only takes 2 trials, so it is used in-between runs and for catch-trial
+#placement to also remove perceptual continuity there
 def validate_grouping2(input_list):
     number, side = input_list[0].split('_')
     number2, side = input_list[1].split('_')
@@ -90,8 +101,8 @@ for sheet in range(1, totalSheets + 1):
     for run in range(1, num_runs + 1):
         conditions = conditions[:]
         random.shuffle(conditions)  # Shuffle conditions for each run
-        #Ensures that the condition at the end of run is not the condition at the start of the next run
-        #This would be a catch trial, but I want to determine all catch trials later
+        #Ensures that no trial in the run stars where a previous trial ended, and that the first trial in the run
+        #Does not start where the last trial in the previous run ended
         while (validate_grouping1(conditions, grouped_list) == True) or validate_grouping2([conditionsOld[-1], conditions[0]]) == True:
             random.shuffle(conditions)
         #while conditionsOld[-1] == conditions[0]:
@@ -132,7 +143,8 @@ for sheet in range(1, totalSheets + 1):
         duplicated_row = df.iloc[original_index].copy()
         duplicated_row['Catch'] = 1
         catchCond = random.choice(conditions)
-        # Correct the while loop condition
+        # Before inserting the catch trial, make sure that the previous trial and the subsequent trial
+        #Do not start where this trial ends
         while validate_grouping2([catchCond, df.iloc[original_index]['Condition']]) or validate_grouping2([catchCond, df.iloc[original_index + 1]['Condition']]):
             catchCond = random.choice(conditions)
         duplicated_row['Condition'] = catchCond
